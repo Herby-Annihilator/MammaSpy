8224efcf1f158830886fd5c74da725b7b6e9e422
Rukin Danil <rukin2018d@yandex.ru>
2021-03-21 16:55:02 +0700

    Настраиваем окошко на тест данных

---
+++ Model/Security.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MammaSpy.Model
+{
+	internal static class Security
+	{
+		/// <summary>
+		/// Заглушка, ну тип, надо же показать, что не все так просто у нас тут
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		internal static string Encrypt(string value)
+		{
+			return value;
+		}
+	}
+}
